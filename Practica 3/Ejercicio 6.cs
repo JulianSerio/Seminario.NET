1366e897c71f8d945b279b0c1989f84d9b886707
Julián Serio <83135918+JulianSerio@users.noreply.github.com>
2024-03-22 16:55:49 -0300

    Add files via upload

---
+++ Practica 3/Ejercicio 6.cs
@@ -0,0 +1,54 @@
+﻿double [,]? a = new double[2,2];
+double [,]? b = new double[2,2];
+int filasA = a.GetLength(0);
+int columnasA = a.GetLength(1);
+int filasB = b.GetLength(0);
+int columnasB = b.GetLength(1);
+
+if (filasA + columnasA != filasB + columnasB){
+    throw new ArgumentException("Las matrices no son iguales!");
+}
+
+double[,]? Suma(double[,] A, double[,] B){
+    if (filasA + columnasA == filasB + columnasB){
+        double [,]? m = new double[filasA,columnasA];
+        for (int i = 0; i < filasA; i++){
+            for (int j = 0; j < columnasA; j++){
+                m[i,j] = A[i,j] + B[i,j];
+            }
+        }
+        return m;
+    }   
+    else {
+        return null;
+    }
+}
+double[,]? Resta(double[,] A, double[,] B){
+    if (filasA + columnasA == filasB + columnasB){
+        double [,]? m = new double[filasA,columnasA];
+        for (int i = 0; i < filasA; i++){
+            for (int j = 0; j < columnasA; j++){
+                m[i,j] = A[i,j] - B[i,j];
+            }
+        }
+        return m;
+    }   
+    else {
+        return null;
+    }
+}
+double[,] Multiplicacion(double[,] A, double[,] B){
+    if (columnasA == filasB){
+        double [,]? m = new double[filasA,columnasA];
+        for (int i = 0; i < filasA; i++){
+            for (int j = 0; j < columnasA; j++){
+                m[i,j] = A[i,j] * B[i,j];
+            }
+        }
+        return m;
+    }   
+    else{
+        throw new ArgumentException("Las matrices no son iguales");
+    }
+
+}
